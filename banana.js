59d8bb7f8ea48d340a4fc35fd8e6545644f7a719
SilverSpiritUK <30852309+SilverSpiritUK@users.noreply.github.com>
2018-02-17 04:13:24 +0000

    Uploaded Files
    Added the files.

---
+++ banana.js
@@ -0,0 +1,36 @@
+window.onload = function() {
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    var img = document.createElement("img");
+    img.src = "banana.gif";
+    c.width = img.width*2;
+    c.height = img.height;
+    ctx.width = img.width*2;
+    ctx.height = img.height;
+
+    getGIFData(img);
+
+    var counter = 0;
+    var counter2 = 2;
+    c.onclick = function(){
+      //window.setInterval(doFrame, 110);
+      window.requestAnimationFrame(doFrame);
+    }
+    
+    var doFrame = function(){
+      img.gif.drawFrame(ctx, counter);
+      img.gif.drawFrame(ctx, counter2, img.width, 0);
+    
+      if(++counter >= img.gif.length)
+      {
+         counter = 0;
+      }
+
+      if(++counter2 >= img.gif.length)
+      {
+         counter2 = 0;
+      }
+
+      window.setTimeout(window.requestAnimationFrame(doFrame),img.gif.delay);
+    }
+}
